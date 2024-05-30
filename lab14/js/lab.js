010f16e02406338850f498e498351e18b7a2d487
em <eishida17@gmail.com>
2024-05-30 12:56:56 -0700

    lab 14 + debugging lab 7

---
+++ lab14/js/lab.js
@@ -0,0 +1,10 @@
+// Lab 14: Debugging Tools and Strategies
+// Author: Em Ishida <eishida@ucsc.edu> and Jasmin Vega <jlvega@ucsc.edu>
+// Date: May 30, 2024
+
+// This simple JavaScript/jQuery script returns certain words for each number through from 1-200
+
+// Requirements: jQuery must be loaded for this script to work.
+
+
+
